6a5915fc94e3259924d2b0dca36a73e3b5fc9e83
YevheniiMih <yevhenii_m@optimove.com>
2024-04-09 19:38:20 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Yevhenii M on 09.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
